f0409f46e9c5592a300c5d1bcaeceba917f11a98
sachinmmane <tosachinmane@gmail.com>
2024-06-14 21:12:08 +0530

    feat: Create and manage Department API
    Implemented the following:
    - Added serializer and view for the Department.
    - Added routes for department creation, listing, and deletion.
    - Created app-level URLs and moved user-related URLs to the app-level configuration.
    - Added department-related URLs to the app-level configuration.

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Department(models.Model):
+    # Define the primary key for the Department model as a CharField with a maximum length of 100 characters
+    id = models.CharField(primary_key=True, max_length=100)
+    # Define a name field for the Department model as a CharField with a maximum length of 100 characters
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        # Define the string representation of the Department model to return the name of the department
+        return self.name
