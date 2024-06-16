213edf7ed7061219be39ba588b6dc82cf30062be
sachinmmane <tosachinmane@gmail.com>
2024-06-16 15:57:44 +0530

    chore: moved departments functionality to the Department app for better code maintainability

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -3,8 +3,6 @@ from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('departments/', views.DepartmentListCreate.as_view(), name='department-list'),
-    path('departments/delete/<int:pk>', views.DepartmentListCreate.as_view(), name='delete-department'),
     path('user/register/', views.CreateUserView.as_view(), name='register'),
     path('token', TokenObtainPairView.as_view(), name='get_token'),
     path('token/refresh', TokenRefreshView.as_view(), name='refresh'),
